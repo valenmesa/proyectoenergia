24c15f19d5e583594f1c32206cde0e48d4d87d72
valenmesa <valentinamesa189@gmail.com>
2023-09-06 10:05:14 -0500

    settings

--- proyectoEnergia/settings.py
+++ proyectoEnergia/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'proyectoEnergia/static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 LOGIN_REDIRECT_URL = reverse_lazy('profesor_listar')
 LOGOUT_REDIRECT_URL = reverse_lazy('login')
 
