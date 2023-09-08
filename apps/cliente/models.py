77ec9061dc88de955bd4c75e210a98b8d98732f3
valenmesa <valentinamesa189@gmail.com>
2023-09-08 08:49:46 -0500

    inactivar cliente

--- apps/cliente/models.py
+++ apps/cliente/models.py
@@ -8,7 +8,7 @@ class Cliente(models.Model):
     #atributos
     nombre_cliente=models.CharField(max_length=50)
     apellido_cliente=models.CharField(max_length=70)
-    telefono_cliente=models.BigIntegerField(max_length=15)
+    telefono_cliente=models.BigIntegerField()
     email_cliente=models.EmailField()
     direccion_cliente=models.CharField(max_length=40)
     tipo_documento=models.ForeignKey(Tipo_Documento, null=True, blank=True, on_delete=models.CASCADE)
