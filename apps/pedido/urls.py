05954dff3d129e09607dec059776e98307a523c3
Karenjoha <karenjoha3838@gmail.com>
2023-09-14 06:27:10 -0500

    cambio

--- apps/pedido/urls.py
+++ apps/pedido/urls.py
@@ -1,10 +1,11 @@
-from  django.urls import re_path, include,path
+from  django.urls import re_path, include, path
 from apps.pedido.views import PedidoView, pedidos, ComprasDelete
-
+from .reportes import reporte_compras, imprimir_compra
 urlpatterns = [
-
     path(r'listar/', PedidoView.as_view(), name='pedido_listar'),
     path(r'nuevo/', pedidos, name='pedido_crear'),
     path(r'pedido/editar/<int:compra_id>', pedidos, name='pedido_editar'),
     path(r'pedido/<int:compra_id>/delete/<int:pk>', ComprasDelete.as_view(), name='compras_del'),
+    path(r'pedido/listar', reporte_compras, name='pedido_print_all'),
+    path(r'pedido/editar/<int:compra_id/imprimir>', imprimir_compra, name='compras_print_one'),
 ]
